10aa8251102b1aca779f267e061666e66fd7bbc9
Brent Watson <brentmwatson@gmail.com>
2016-10-05 17:30:00 -0400

    explorer mode finished

--- robots.rb
+++ robots.rb
@@ -12,7 +12,7 @@ attr_accessor :name
 #   "Roberto"
 # end
 # OR self.name = name || "Roberto"
-  def initialize (name)
+  def initialize (name = "Roberto")
     @name = name
   end
 # A robot instance should have a method called 'say_hi' and it should return "Hi!"
@@ -21,9 +21,10 @@ attr_accessor :name
   end
 
   # A robot instance should have a method called 'say_name' and it should return "My name is X" where X is the robot's name
-  def say_name(text="I'm Craaaaaaaaaaaaaaaaaaaaaazy")
-    system ("say My name is #{self.name} #{text}.")
+  def say_name(text="I'm Cra a eeeeeeeeeee zeeeeeeeeeeey")
+    "My name is #{self.name} #{text}."
   end
+end
 
 # Define a BendingUnit class
 # A BendingUnit inherits from Robot
@@ -32,9 +33,6 @@ class BendingUnit < Robot
   # A bending unit instance has a method called 'bend'
   # The bend method has one argument??? 'objecttobend'
   # The bend method should put to the console "Bend X!" where X is objecttobend
-  def initialize(object)
-    self.objecttobend = object
-  end
 
   def bend (object)
    "Bend #{object}!"
@@ -57,7 +55,9 @@ name = Robot.new(name)
 
 # Output an index.html when running ruby robots.rb <name> <object>. Said page should use a BendingUnit with a name of <name> and use that BendingUnit to say hi, say their name and bend <object> and output all the above to HTML at <robot_name>.
 #html where <robot_name> is the <name> given on the command line.
-robot_name = ARGV[0]
+robot_name = BendingUnit.new(ARGV[0])
+
+object = ARGV[1]
 
 new_file = File.open("./#{robot_name}.html", "w+")
 
@@ -66,4 +66,3 @@ new_file << ERB.new(File.read("index.html.erb")).result(binding)
 
 # close file
 new_file.close
-end
